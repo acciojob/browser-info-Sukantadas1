5deff6b3fc1e74ee0ecacd99664bccba3e2b3c69
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-23 15:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    // Get browser information from the navigator object
+    const browserName = navigator.appName;
+    const browserVersion = navigator.appVersion;
+
+    // Create a message string
+    const message = "You are using " + browserName + " version " + browserVersion;
+
+    // Display the message inside the div with the id "browser-info"
+    const browserInfoDiv = document.getElementById("browser-info");
+    browserInfoDiv.textContent = message;
+});
